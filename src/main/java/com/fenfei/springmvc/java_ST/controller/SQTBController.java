d51ef8e731481784e1695b95d1ccfbb3180b0a1b
fenfeilove <981239826@qq.com>
2015-07-18 16:13:55 +0800

    解决json字符串乱码问题

--- src/main/java/com/fenfei/springmvc/java_ST/controller/SQTBController.java
+++ src/main/java/com/fenfei/springmvc/java_ST/controller/SQTBController.java
@@ -2,13 +2,9 @@ package com.fenfei.springmvc.java_ST.controller;
 
 import java.util.List;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.fenfei.springmvc.java_ST.service.IApplyService;
@@ -19,21 +15,21 @@ import com.fenfei.springmvc.java_ST.service.IApplyService;
 public class SQTBController {
 	@Autowired
 	IApplyService applyService;
-	@RequestMapping(value="/trees", method = RequestMethod.GET)
+	@RequestMapping(value="/trees", produces = "application/json; charset=utf-8")
 	@ResponseBody
-	public String test(HttpServletRequest request,HttpServletResponse response)
+	public String test()
 	{
-		response.setContentType("text/json");
 		String tree="";
-		tree="[{id:0 , item:[";
+		tree="{id:0 , item:[";
 		List<String> orglist=applyService.getDisPK_ORG();
 		for(int i=0;i<orglist.size();i++)
 		{
-			System.out.println(orglist.get(i));
+			//System.out.println(orglist.get(i));
 			if(i!=0)tree+=",";
-			tree+="{id:\""+orglist.get(i)+"\",text:\""+orglist.get(i)+"\"}";
+			int j=i+1;
+			tree+="{id:"+j+",text:\""+orglist.get(i)+"\",item[]}";
 		}
-		tree+="]}]";
+		tree+="]}";
 		return tree;
 	}
 }
